caf7f7bf43e3d794d729fe616dd18d784e65d702
KisaragiIchigo <stiz1984@gmail.com>
2025-10-07 07:21:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import sys
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication
-import gui  
+import gui
 
 if __name__ == "__main__":
-    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
-    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
-
     app = QApplication(sys.argv)
     app.setFont(QFont("メイリオ", 10))
 
